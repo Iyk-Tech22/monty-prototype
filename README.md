b5dbcd4404a85bc8ab477c9b41068b0a15ea0742
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-10-04 03:48:00 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Monty Project
+Description: simple CLI program takes a
+file of bytecode and validate it.
+Authour:Iyk Faiz
---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Struct stack_s - Stack Data Structure use to store
+ * @n: Integer
+ * @prev: Point to prev node
+ * @next: Point to next node
+ */
+struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+/**
+ * struct instructon_s - opcode function
+ * @opcode: name of instruction
+ * @f: function for opcode instruction
+ */
+struct instruction_s
+{
+	char *opcode;
+	(void *) f(stack_t **stack, unsigned int line number)
+} instruction_t;
